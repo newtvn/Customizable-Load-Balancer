de4136f6a5a5b16d53ebe1b0825c9e3e50f5ddba
newtvn <newtonbryan12428@gmail.com>
2024-04-22 17:58:43 +0300

    Initial commit with project files

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify # type: ignore
+import os
+
+app = Flask(__name__)
+
+@app.route('/home', methods=['GET'])
+def home():
+    server_id = os.environ.get('SERVER_ID', '1')
+    return jsonify(message=f"Hello from Server: {server_id}", status="successful"), 200
+
+@app.route('/test', methods=['GET'])
+def test():
+    server_id = os.environ.get('SERVER_ID', '1')
+    return jsonify(message=f"ooops: {server_id}", status="successful"), 200
+
+@app.route('/heartbeat', methods=['GET'])
+def heartbeat():
+    return '', 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
